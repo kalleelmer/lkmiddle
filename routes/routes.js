17d7a0c9f0e3af3af90309b97c5ddc4e825242d9
Kristoffer <kristoffer.ipod@gmail.com>
2017-11-29 13:05:09 +0100

    fler routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,12 @@ const config = require("../config.json");
 
 module.exports = function(app) {
 
-  app.route(config.prefix + '/show').get(show.getShows);
+  app.route(config.prefix + '/shows').get(show.getShows);
+
+  app.route(config.prefix + '/shows/:id/categories').get(show.getCategories);
+
+  app.route(config.prefix + '/shows/:id/performances').get(show.getPerformances);
+
+  app.route(config.prefix + '/shows/:id/rates').get(show.getRates);
 
 };
